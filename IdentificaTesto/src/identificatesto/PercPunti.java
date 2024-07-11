16da09039bed5ec9aafdc3a3148631994595ffe9
Memma <132008224+MeMMa137@users.noreply.github.com>
2024-07-11 12:01:32 +0200

    Class for percentage calculation of dots

---
+++ IdentificaTesto/src/identificatesto/PercPunti.java
@@ -0,0 +1,21 @@
+package identificatesto;
+import java.util.LinkedList;
+
+class PercPunti {
+    private LinkedList<String> lt;
+    PercPunti(LinkedList<String> lt) {
+        this.lt=lt;
+    }
+
+    float calcpercPunti() {
+        int ns=0,n=0;
+        for(int i=1;i<lt.size();i++) {
+            char[] riga = lt.get(i).toCharArray();
+            for(int j=0;j<riga.length;j++)
+                if(riga[j]=='.')
+                    ns++;
+            n+=riga.length;
+        }
+        return (float)ns/n;
+    }   
+}
